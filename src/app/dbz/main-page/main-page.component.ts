6aee0c687f6ab6b75a53c727af97c85c9e1b3d11
Eduardo Passano Chávez <eapch21@gmail.com>
2022-11-19 23:26:54 -0500

    60. ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Personaje{
+  nombre:string;
+  poder:number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent  {
+
+    nuevo:Personaje={
+      nombre:'Trucks',
+      poder:14000
+    }
     agregar(){
-      console.log('Esta es una prueba');
+      console.log(this.nuevo);
     }
     
 }
